18e17c6d03183ef595aa42e5135a6027dee691fd
pancawibawa <pancawibawa22@gmail.com>
2024-10-30 09:06:05 +0700

    update

--- resources/views/email/interviewNotification.blade.php
+++ resources/views/email/interviewNotification.blade.php
@@ -7,7 +7,7 @@ Terima kasih telah mendaftar sebagai **{{ $pendaftar->job->job_name }}** di peru
 
 ### Status Pendaftaran Diperbarui 🔔
 
-Pendaftaran Anda kini memiliki status **"Interview"**. Berikut adalah informasi lengkap mengenai jadwal tes Anda:
+Pendaftaran Anda kini memiliki status **"Interview"**. Berikut adalah informasi lengkap mengenai jadwal interview Anda:
 
 @component('mail::panel')
 **Tanggal Interview:**  
@@ -16,14 +16,21 @@ Pendaftaran Anda kini memiliki status **"Interview"**. Berikut adalah informasi 
 
 ---
 
-Mohon untuk hadir tepat waktu dan mempersiapkan diri dengan baik. Jangan lupa membawa dokumen yang diperlukan dan mengenakan pakaian yang rapi.
+#### Dokumen yang Wajib Dibawa:
+Mohon untuk membawa berkas-berkas berikut dalam bentuk hardcopy:
+- **Curriculum Vitae (CV)**
+- **Surat Keterangan Catatan Kepolisian (SKCK)**
+- **Fotokopi NPWP**
+- **Fotokopi KTP**
+
+Harap datang tepat waktu dan mempersiapkan diri dengan baik. Kenakan pakaian yang rapi untuk interview.
 
 @component('mail::button', ['url' => config('app.url') . '/dashboard', 'color' => 'success'])
 Lihat Jadwal di Aplikasi
 @endcomponent
 
 @component('mail::subcopy')
-Jangan Lupa Jaga Kesehatan dan Tetap Semangat!
+Jaga kesehatan dan tetap semangat untuk interview!
 @endcomponent
 
 Salam Hormat,  
